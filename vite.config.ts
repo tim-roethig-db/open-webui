25c06a4fc4a658df59819a178b221d189069a3d6
tro16 <tim.roethig@yahoo.de>
2024-10-05 16:07:17 +0200

    base url first try

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	// base: '/test123/',
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
